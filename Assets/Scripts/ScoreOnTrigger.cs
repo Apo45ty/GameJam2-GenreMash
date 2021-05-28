dd87a3e4c646734534718d33b593c85b4acaf3a3
Apo45ty <antonio.tapia@upr.edu>
2021-05-27 20:42:55 -0400

    Final Push

--- Assets/Scripts/ScoreOnTrigger.cs
+++ Assets/Scripts/ScoreOnTrigger.cs
@@ -8,15 +8,22 @@ public class ScoreOnTrigger : MonoBehaviour
     private ScoreManager scoreManager;
     [SerializeField]
     private Teams team;
+    private AudioSource audio;
+    void Start(){
+        audio = GetComponent<AudioSource>();
+    }
 
      private void OnTriggerEnter(Collider other)
     {
         if(other.tag=="Ball"){
+            if(audio)
+            audio.Play();
             scoreManager.addScoreToTeam(1,team);
             other.transform.position = new Vector3(0,16.8f,0);
             Rigidbody rigidbody1 = other.GetComponent<Rigidbody>();
             if(rigidbody1)
                 rigidbody1.velocity=Vector3.zero;
+            
         }
     }
 }
